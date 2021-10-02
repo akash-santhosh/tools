1fac8faa2a18b41243bcb63fe7c958f1e84f9473
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-10-03 00:58:26 +0530

    added sites form pull#13

--- data.js
+++ data.js
@@ -8,5 +8,29 @@ export const data=[
         "siteName":"Sentry",
         "siteURL":"https://sentry.io",
         "siteDesc":"Application monitoring and error tracking software"
-    }
+    },
+    {
+        "siteName":"Figma",
+        "siteURL":"https://www.figma.com/",
+        "siteDesc":"Web-based Vector Graphics editor & Prototyping tool"
+    },
+    {
+        "siteName":"FigJam",
+        "siteURL":"https://www.figma.com/figjam/",
+        "siteDesc":"Whiteboard collaboration tool to brainstorm and organize ideas"
+    },
+    {
+        "siteName":"Coolors",
+        "siteURL":"https://coolors.co",
+        "siteDesc":"Color Palettes Generator for Designers and Artists"
+    },
+    {
+        "siteName":"Color Hunt",
+        "siteURL":"https://colorhunt.co/",
+        "siteDesc":"Color Palettes Generator for Designers and Artists"
+    },{
+        "siteName":"Creately",
+        "siteURL":"https://creately.com/",
+        "siteDesc":"Collaboration tool with diagramming & design capabilities"
+    },
 ]
