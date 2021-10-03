60a9b200ee2e0e92151edd55c5368f2e60faf715
unknown <alankruthisaieni@gmail.com>
2021-10-03 23:10:57 +0530

    AddTool

--- data.js
+++ data.js
@@ -1,44 +1,50 @@
-export const categories = ["Frontend","Others","Design"];
-export const data=[
+export const categories = ["Frontend", "Others", "Design"];
+export const data = [
     {
-        "siteName":"Hoppscotch",
-        "siteURL":"https://hoppscotch.io",
-        "siteDesc":"An Open source API development ecosystem",
-        "category":"Others"
+        "siteName": "Hoppscotch",
+        "siteURL": "https://hoppscotch.io",
+        "siteDesc": "An Open source API development ecosystem",
+        "category": "Others"
     },
     {
-        "siteName":"Sentry",
-        "siteURL":"https://sentry.io",
-        "siteDesc":"Application monitoring and error tracking software",
-        "category":"Others"
+        "siteName": "Sentry",
+        "siteURL": "https://sentry.io",
+        "siteDesc": "Application monitoring and error tracking software",
+        "category": "Others"
     },
     {
-        "siteName":"Figma",
-        "siteURL":"https://www.figma.com/",
-        "siteDesc":"Web-based Vector Graphics editor & Prototyping tool",
-        "category":"Design"
+        "siteName": "Figma",
+        "siteURL": "https://www.figma.com/",
+        "siteDesc": "Web-based Vector Graphics editor & Prototyping tool",
+        "category": "Design"
     },
     {
-        "siteName":"FigJam",
-        "siteURL":"https://www.figma.com/figjam/",
-        "siteDesc":"Whiteboard collaboration tool to brainstorm and organize ideas",
-        "category":"Others"
+        "siteName": "FigJam",
+        "siteURL": "https://www.figma.com/figjam/",
+        "siteDesc": "Whiteboard collaboration tool to brainstorm and organize ideas",
+        "category": "Others"
     },
     {
-        "siteName":"Coolors",
-        "siteURL":"https://coolors.co",
-        "siteDesc":"Color Palettes Generator for Designers and Artists",
-        "category":"Frontend"
+        "siteName": "Coolors",
+        "siteURL": "https://coolors.co",
+        "siteDesc": "Color Palettes Generator for Designers and Artists",
+        "category": "Frontend"
     },
     {
-        "siteName":"Color Hunt",
-        "siteURL":"https://colorhunt.co/",
-        "siteDesc":"Color Palettes Generator for Designers and Artists",
-        "category":"Frontend"
-    },{
-        "siteName":"Creately",
-        "siteURL":"https://creately.com/",
-        "siteDesc":"Collaboration tool with diagramming & design capabilities",
-        "category":"Others"
+        "siteName": "Color Hunt",
+        "siteURL": "https://colorhunt.co/",
+        "siteDesc": "Color Palettes Generator for Designers and Artists",
+        "category": "Frontend"
+    }, {
+        "siteName": "Creately",
+        "siteURL": "https://creately.com/",
+        "siteDesc": "Collaboration tool with diagramming & design capabilities",
+        "category": "Others"
     },
+    {
+        "siteName": "RapidAPI",
+        "siteURL": "https://rapidapi.com/hub",
+        "siteDesc": "To discover and connect to thousands of APIs",
+        "category": "Others"
+    }
 ]
