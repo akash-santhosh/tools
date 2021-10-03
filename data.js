0c4e3b2946b33c62418fdcc188862d1ab07a66b2
unknown <alankruthisaieni@gmail.com>
2021-10-03 12:55:25 +0530

    Added Site

--- data.js
+++ data.js
@@ -1,36 +1,41 @@
-export const data=[
+export const data = [
     {
-        "siteName":"Hoppscotch",
-        "siteURL":"https://hoppscotch.io",
-        "siteDesc":"An Open source API development ecosystem"
+        "siteName": "Hoppscotch",
+        "siteURL": "https://hoppscotch.io",
+        "siteDesc": "An Open source API development ecosystem"
     },
     {
-        "siteName":"Sentry",
-        "siteURL":"https://sentry.io",
-        "siteDesc":"Application monitoring and error tracking software"
+        "siteName": "Sentry",
+        "siteURL": "https://sentry.io",
+        "siteDesc": "Application monitoring and error tracking software"
     },
     {
-        "siteName":"Figma",
-        "siteURL":"https://www.figma.com/",
-        "siteDesc":"Web-based Vector Graphics editor & Prototyping tool"
+        "siteName": "Figma",
+        "siteURL": "https://www.figma.com/",
+        "siteDesc": "Web-based Vector Graphics editor & Prototyping tool"
     },
     {
-        "siteName":"FigJam",
-        "siteURL":"https://www.figma.com/figjam/",
-        "siteDesc":"Whiteboard collaboration tool to brainstorm and organize ideas"
+        "siteName": "FigJam",
+        "siteURL": "https://www.figma.com/figjam/",
+        "siteDesc": "Whiteboard collaboration tool to brainstorm and organize ideas"
     },
     {
-        "siteName":"Coolors",
-        "siteURL":"https://coolors.co",
-        "siteDesc":"Color Palettes Generator for Designers and Artists"
+        "siteName": "Coolors",
+        "siteURL": "https://coolors.co",
+        "siteDesc": "Color Palettes Generator for Designers and Artists"
     },
     {
-        "siteName":"Color Hunt",
-        "siteURL":"https://colorhunt.co/",
-        "siteDesc":"Color Palettes Generator for Designers and Artists"
-    },{
-        "siteName":"Creately",
-        "siteURL":"https://creately.com/",
-        "siteDesc":"Collaboration tool with diagramming & design capabilities"
+        "siteName": "Color Hunt",
+        "siteURL": "https://colorhunt.co/",
+        "siteDesc": "Color Palettes Generator for Designers and Artists"
+    }, {
+        "siteName": "Creately",
+        "siteURL": "https://creately.com/",
+        "siteDesc": "Collaboration tool with diagramming & design capabilities"
     },
+    {
+        "siteName": "RapidAPI",
+        "siteURL": "https://rapidapi.com/hub",
+        "siteDesc": "To discover and connect to thousands of APIs"
+    }
 ]
