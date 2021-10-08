a8c2f2de135049c2c7200d6facd6c9435275c154
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-08 14:59:27 +0530

    Update data.js

--- data.js
+++ data.js
@@ -136,7 +136,8 @@ export const data=[
         "siteURL":"https://www.blobmaker.app/",
         "siteDesc":"Free generative design tool to help you quickly create random, unique, and organic-looking SVG shape.",
         "category":"Design"
-    }
+    },
+    {
         "siteName":"Undraw",
         "siteURL":"https://undraw.co/",
         "siteDesc":"Free and open-source illustrations for any idea you can imagine and create.",
