b42109f4640cb47a1cfb912325d5c5ac7bb74a04
NujoomR <72660555+NujoomR@users.noreply.github.com>
2021-10-09 16:26:27 +0530

    Update data.js

--- data.js
+++ data.js
@@ -227,5 +227,11 @@ export const data=[
         "siteURL":"https://sass-lang.com/",
         "siteDesc":"Sass is the most mature, stable, and powerful professional grade CSS extension language in the world.",
         "category":"Development"
+    },
+    {
+        "siteName": "Figma",
+        "siteURL": "https://www.figma.com/",
+        "siteDesc": "Web-based Vector Graphics editor & Prototyping tool",
+        "category": "Design"
     }
 ]
